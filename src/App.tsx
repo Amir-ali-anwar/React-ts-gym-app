346891ea572bf84b82a31abb710bd61fe167be0c
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-12-25 16:22:03 +0500

    working on the navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
   
 function App() {
-  return (
-    <div className="App">
-      asdasd
-    </div>
-  )
+  return <div className="App gray-50 secondary-500">asdasd</div>;
 }
 
 export default App
