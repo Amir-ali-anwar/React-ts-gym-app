0467c2b9acfe8dbabeaee2ce19d5c53de7585d2f
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-01-03 00:07:18 +0500

    added home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import Navbar from "@/components/Navbar";
-import { useState } from "react";
-import {SelectedPage} from "@/shared/types"  
+import { useState, useEffect } from "react";
+import { SelectedPage } from "@/shared/types";
+
 function App() {
-  const [selectedPage,setSelectedPage]= useState<string>(SelectedPage.Home)
-  return <div className="App gray-50 secondary-500">
-    <Navbar setSelectedPage={setSelectedPage} selectedPage={selectedPage} /> 
-  </div>;
+  const [selectedPage, setSelectedPage] = useState<string>(SelectedPage.Home);
+  const [TopScroll, SetTopScroll] = useState<boolean>(true);
+  const handleScroll = () => {
+    if (window.screenY === 0) {
+      SetTopScroll(true);
+      setSelectedPage(SelectedPage.Home);
+    }
+    if (window.screenY !== 0) {
+      SetTopScroll(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="App gray-50 secondary-500">
+      <Navbar setSelectedPage={setSelectedPage} selectedPage={selectedPage} TopScroll={TopScroll} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,19 +4,21 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import ActionBtton from "../ActionBtton";
 import useMediaQuery from "@/Hooks/useMediaQueries";
-useState
+useState;
 type Props = {
   selectedPage: string;
   setSelectedPage: (value: string) => void;
+  TopScroll:boolean
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ TopScroll,selectedPage, setSelectedPage }: Props) => {
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   const flexBetween = "flex items-center justify-between";
+  const navbarbackgrond= TopScroll?  "" : "bg-primary-100 drop-shadow";
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${navbarbackgrond} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img alt="logo" src={Logo} />
@@ -62,6 +64,39 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+      {/* Mobile sidebar */}
+      { !isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          {/* MENU ITEMS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
