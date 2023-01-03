6e0ab13ae88343a39579466d4159474c38d54fa6
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-01-03 21:36:48 +0500

    working on the banner section

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -15,7 +15,41 @@ type Props = {
 const Home = ({setSelectedPage}: Props) => {
     const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
   return (
-    <div>index</div>
+    <section id='home' className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* Image and main header */}
+      <div className='md:flex mx-auto'>
+        {/* Main header */}
+        <div>
+          {/* Headings */}
+          <div>
+            <div>
+            <img alt="home-page-text" src={HomePageText} />
+            </div>
+          </div>
+          <p>  Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.</p>
+        </div>
+        <div>
+          <ActionBtton setSelectedPage={setSelectedPage}>
+              Join Now
+          </ActionBtton>
+          <AnchorLink   className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+          onClick={()=>setSelectedPage(SelectedPage.ContactUs)}
+          href={`#${SelectedPage.ContactUs}`}
+          >
+          <p>Learn More</p>
+          </AnchorLink>
+        </div>
+         {/* IMAGE */}
+         <div
+          className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
+        </div>
+      </div>
+    </section>
   )
 }
 
