d2e5cbb32a7a03ceacda0479c3f301ae64598916
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-01-03 22:20:26 +0500

    working on the banner action

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -8,6 +8,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -17,35 +18,56 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* Image and main header */}
-      <div className="mx-auto w-5/6 items-center justify-center md:flex">
+      <motion.div className="mx-auto w-5/6 items-center justify-center md:flex">
         {/* Main header */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* Headings */}
-          <div className="md:-mt-20 ">
+          <motion.div className="md:-mt-20 "
+           initial="hidden"
+           whileInView="visible"
+           viewport={{once:true,amount:0.5}} 
+           transition={{duration:0.5}}
+           variants={{
+            hidden:{opacity:0,x:-50},
+            visible:{opacity:1, x:0}
+            
+           }}
+          
+          >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-evolvetext" >
                 <img alt="home-page-text" src={HomePageText} />
               </div>
             </div>
-          </div>
-          <div>
-          <p className="mt-8 text-sm">
-            {" "}
-            Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
-            Studios to get the Body Shapes That you Dream of.. Get Your Dream
-            Body Now.
-          </p>
-          <div className="mt-8 flex items-center gap-8">
-          <ActionBtton setSelectedPage={setSelectedPage}>Join Now</ActionBtton>
-          <AnchorLink
-            className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-            href={`#${SelectedPage.ContactUs}`}
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </motion.div>
+          {/* Action buttons */}
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
           >
-            <p>Learn More</p>
-          </AnchorLink>
-        </div>
-        </div>
+            <ActionBtton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionBtton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
         </div>
        
         {/* IMAGE */}
@@ -55,7 +77,7 @@ const Home = ({ setSelectedPage }: Props) => {
         >
           <img alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
-      </div>
+      </motion.div>
       {
         isAboveMediumScreens && (
           <div className="h-[150px] w-full bg-primary-100 py-10">
